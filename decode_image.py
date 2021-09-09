bd8aca02e4a2538252b200e63074df558650ccc4
丁稼宇 <1501793890@qq.com>
2021-09-09 16:41:25 +0800

    encode&decode

--- decode_image.py
+++ decode_image.py
@@ -3,9 +3,11 @@ import bchlib
 import numpy as np
 from PIL import Image, ImageOps
 import torch
+from torchvision import transforms
 
 BCH_POLYNOMIAL = 137
 BCH_BITS = 5
+
 
 def main():
     import argparse
@@ -14,6 +16,7 @@ def main():
     parser.add_argument('--image', type=str, default=None)
     parser.add_argument('--images_dir', type=str, default=None)
     parser.add_argument('--secret_size', type=int, default=100)
+    parser.add_argument('--cuda', type=bool, default=True)
     args = parser.parse_args()
 
     if args.image is not None:
@@ -25,32 +28,47 @@ def main():
         return
 
     decoder = torch.load(args.model)
+    decoder.eval()
+    if args.cuda:
+        decoder = decoder.cuda()
 
     bch = bchlib.BCH(BCH_POLYNOMIAL, BCH_BITS)
 
-    for filename in files_list:
-        image = Image.open(filename).convert("RGB")
-        image = np.array(ImageOps.fit(image,(400, 400)),dtype=np.float32)
-        image /= 255.
+    width = 400
+    height = 400
+    size = (width, height)
+    to_tensor = transforms.ToTensor()
 
-        secret = decoder(image)
+    with torch.no_grad():
+        for filename in files_list:
+            image = Image.open(filename).convert("RGB")
+            image = ImageOps.fit(image, size)
+            image = to_tensor(image).unsqueeze(0)
+            if args.cuda:
+                image = image.cuda()
 
-        packet_binary = "".join([str(int(bit)) for bit in secret[:96]])
-        packet = bytes(int(packet_binary[i : i + 8], 2) for i in range(0, len(packet_binary), 8))
-        packet = bytearray(packet)
+            secret = decoder(image)
+            if args.cuda:
+                secret = secret.cpu()
+            secret = np.array(secret[0])
+            secret = np.round(secret)
 
-        data, ecc = packet[:-bch.ecc_bytes], packet[-bch.ecc_bytes:]
+            packet_binary = "".join([str(int(bit)) for bit in secret[:96]])
+            packet = bytes(int(packet_binary[i: i + 8], 2) for i in range(0, len(packet_binary), 8))
+            packet = bytearray(packet)
 
-        bitflips = bch.decode_inplace(data, ecc)
+            data, ecc = packet[:-bch.ecc_bytes], packet[-bch.ecc_bytes:]
 
-        if bitflips != -1:
-            try:
-                code = data.decode("utf-8")
-                print(filename, code)
-                continue
-            except:
-                continue
-        print(filename, 'Failed to decode')
+            bitflips = bch.decode_inplace(data, ecc)
+
+            if bitflips != -1:
+                try:
+                    code = data.decode("utf-8")
+                    print(filename, code)
+                    continue
+                except:
+                    continue
+            print(filename, 'Failed to decode')
 
 
 if __name__ == "__main__":
--- encode_image.py
+++ encode_image.py
@@ -1,14 +1,15 @@
-
 import os
 import glob
 import bchlib
 import numpy as np
-from PIL import Image,ImageOps
+from PIL import Image, ImageOps
 
 import torch
+from torchvision import transforms
 
 BCH_POLYNOMIAL = 137
 BCH_BITS = 5
+
 
 def main():
     import argparse
@@ -16,9 +17,10 @@ def main():
     parser.add_argument('model', type=str)
     parser.add_argument('--image', type=str, default=None)
     parser.add_argument('--images_dir', type=str, default=None)
-    parser.add_argument('--save_dir', type=str, default=None)
+    parser.add_argument('--save_dir', type=str, default=r'./images')
     parser.add_argument('--secret', type=str, default='Stega!!')
     parser.add_argument('--secret_size', type=int, default=100)
+    parser.add_argument('--cuda', type=bool, default=True)
     args = parser.parse_args()
 
     if args.image is not None:
@@ -29,10 +31,10 @@ def main():
         print('Missing input image')
         return
 
-    encoder = torch.load(args.model).cuda()
-
-    width = 400
-    height = 400
+    encoder = torch.load(args.model)
+    encoder.eval()
+    if args.cuda:
+        encoder = encoder.cuda()
 
     bch = bchlib.BCH(BCH_POLYNOMIAL, BCH_BITS)
 
@@ -40,38 +42,52 @@ def main():
         print('Error: Can only encode 56bits (7 characters) with ECC')
         return
 
-    data = bytearray(args.secret + ' '*(7-len(args.secret)), 'utf-8')
+    data = bytearray(args.secret + ' ' * (7 - len(args.secret)), 'utf-8')
     ecc = bch.encode(data)
     packet = data + ecc
 
     packet_binary = ''.join(format(x, '08b') for x in packet)
     secret = [int(x) for x in packet_binary]
-    secret.extend([0,0,0,0])
+    secret.extend([0, 0, 0, 0])
+    secret = torch.tensor(secret, dtype=torch.float).unsqueeze(0)
+    if args.cuda:
+        secret = secret.cuda()
+
+    width = 400
+    height = 400
+    size = (width, height)
+    to_tensor = transforms.ToTensor()
 
     if args.save_dir is not None:
         if not os.path.exists(args.save_dir):
             os.makedirs(args.save_dir)
-        size = (width, height)
-        for filename in files_list:
-            image = Image.open(filename).convert("RGB")
-            image = np.array(ImageOps.fit(image, size), dtype=np.float32)
-            image /= 255.
 
-            hidden_img, residual = encoder(secret, image)
-            
-            rescaled = (hidden_img[0] * 255).astype(np.uint8)
-            # raw_img = (image * 255).astype(np.uint8)
-            residual = residual[0]+.5
+        with torch.no_grad():
+            for filename in files_list:
+                image = Image.open(filename).convert("RGB")
+                image = ImageOps.fit(image, size)
+                image = to_tensor(image).unsqueeze(0)
+                if args.cuda:
+                    image = image.cuda()
 
-            residual = (residual * 255).astype(np.uint8)
+                residual = encoder((secret, image))
+                encoded = image + residual
+                if args.cuda:
+                    residual = residual.cpu()
+                    encoded = encoded.cpu()
+                encoded = np.array(encoded.squeeze(0) * 255, dtype=np.uint8).transpose((1, 2, 0))
 
-            save_name = filename.split('/')[-1].split('.')[0]
+                residual = residual[0] + .5
+                residual = np.array(residual.squeeze(0) * 255, dtype=np.uint8).transpose((1, 2, 0))
 
-            im = Image.fromarray(np.array(rescaled))
-            im.save(args.save_dir + '/'+save_name+'_hidden.png')
+                save_name = os.path.basename(filename).split('.')[0]
 
-            im = Image.fromarray(np.squeeze(np.array(residual)))
-            im.save(args.save_dir + '/'+save_name+'_residual.png')
+                im = Image.fromarray(encoded)
+                im.save(args.save_dir + '/' + save_name + '_hidden.png')
+
+                im = Image.fromarray(residual)
+                im.save(args.save_dir + '/' + save_name + '_residual.png')
+
 
 if __name__ == "__main__":
     main()
